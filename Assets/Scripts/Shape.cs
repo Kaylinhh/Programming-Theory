8ed966cfcd51326e34aecacbdf6e64824eef2beb
Kaylinhh <ly.julie@live.fr>
2024-11-26 15:41:29 +0100

    set up the Shape Script (parent class)
    added encapsulated properties and a DisplayT'ext() method

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    private string shapeName;
+    private Color shapeColor;
+    public TextMeshProUGUI messageText;
+
+    public string ShapeName
+    {
+        get { return shapeName;  }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                shapeName = value;
+            }
+            else
+            {
+                Debug.Log("The name of the shape can't be empty!");
+                shapeName = "Nameless shape";
+            }
+        }
+    }
+
+    public Color ShapeColor
+    {
+        get { return shapeColor; }
+        set 
+        {
+            shapeColor = value;
+
+            GetComponent<Renderer>().material.color = shapeColor; //changes the color visually
+        }
+    }
+
+
+    public virtual void DisplayText()
+    {
+        if (messageText != null) 
+        {
+            messageText.text = $"I'm a shape named {ShapeName} and I'm {ShapeColor}";
+        }
+        else 
+        {
+            Debug.Log("TextMeshPro not assigned");
+        }
+    }
+}
